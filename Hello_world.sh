712fb53efe1611a66b2599985e6b667caf95f7dd
jamiegpo <jamietulloch@live.co.uk>
2021-02-24 18:22:06 +0000

    Create Hello_world.sh

---
+++ Hello_world.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "Updating apt-get and installing JQ..."
+apt-get -qq update
+apt-get -qq -y install jq >/dev/null 2>&1
+
+echo "Running Python script $script_path/$script_name..."
+python3 Hello_world.sh
